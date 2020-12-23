4c955c3480000ccf4ec52ad61ae06c1eb9186927
vinograd1ka <kachanov220@mail.ua>
2020-12-23 21:22:43 +0200

    2 complete

---
+++ script.js
@@ -0,0 +1,45 @@
+(function () {
+//1
+
+    function isArrayLengthEqual(array1, array2) {
+        return array1.length === array2.length;
+    }
+    console.log(isArrayLengthEqual(['sad', 'asd'] , ['sfgh' , 'wqe' , 'wesda']));
+
+//2
+
+    function arrayHasElement() {
+    }
+
+//3
+
+
+
+//4
+
+    function getBiggestNumber(array) {
+        return Math.max.apply( Math, array);
+    }
+
+    console.log(getBiggestNumber([5 , 122]));
+//5
+
+    function createMultiplier() {
+    }
+
+//6
+
+    function isFunctionNamesEqual() {
+    }
+
+//7
+
+    function substituteElement() {
+    }
+
+//8
+
+    function arraysHasElement() {
+    }
+
+})()
