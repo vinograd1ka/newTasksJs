f14cb5eb69b77a40edf02a4fbaf8e2676f3c6446
vinograd1ka <kachanov220@mail.ua>
2020-12-28 22:50:00 +0200

    complete all except 3 task

--- script.js
+++ script.js
@@ -1,63 +1,104 @@
 (function () {
-//1
+//1 COMPLETE
 
     function isArrayLengthEqual(array1, array2) {
         return array1.length === array2.length;
     }
     console.log(isArrayLengthEqual(['sad', 'asd'] , ['sfgh' , 'wqe' , 'wesda']));
 
-//2
+//2 COMPLETE
 
-    var arrayHasElement = function () {
+    var arrayHasElement = function (array, val) {
+        /*return arr.some(function(arrayVal) {
+            return val === arrayVal;
+        });*/
 
+        for (var i = 0; i < array.length; i++){
+            if (array[i] === val){
+                return 'true';
+            }
+        }
+        return 'false';
     }
-    console.log(arrayHasElement(['true', 'false', 5], 5))
+
+    var result2 = arrayHasElement([1, 'true', 'asdasdasgf', 5], 'true'); // true
+    console.log(result2);
 
 //3
 
-    var getLetterAmountInString= function (string, letter) {
-
+    var getLetterAmountInString= function (array, letter) {
+        //return (string.split(letter).length-1);
+        for (var i = 0; i < array.length; i++){
+            if (array[i] === letter){
+                return array.letter;
+            }
+        }
+        return 'error';
     }
-    console.log(getLetterAmountInString('asgdsgsdghefgdcvzx', 'a'));
+    var result3 = getLetterAmountInString('asadasdasds', 'a');
+    console.log(result3);
 
-//4
+//4 COMPLETE
 
     var getBiggestNumber = function (number0, number1) {
-        //return Math.max.apply( Math, array);
-        var result = '';
         if (number0 > number1){
-            result = number0;
-            return result;
+            return number0;
         }
         else{
-            result = number1;
-            return result;
+            return number1;
         }
     }
-    console.log(getBiggestNumber(12 , 6));
+    var result4 = getBiggestNumber(12 , 15);
+    console.log(result4);
 
-//5
+//5 COMPLETE
 
-    var createMultiplier = function (number) {
-        console.log(number*number);
+    var createMultiplier = function () {
+        var multiplier = function (number) {
+            return number*number;
+        }
+        return multiplier;
     }
 
-    createMultiplier(12);
+    var multiplier = createMultiplier();
+    console.log(multiplier(8));
 
 
-//6
+//6 COMPLETE
 
-    function isFunctionNamesEqual() {
+    var result = function isFunctionNamesEqual (func1, func2) {
+        return func1.name === func2.name;
     }
 
-//7
+    console.log(result(function createMan(){}, function createMan(){}));
 
-    function substituteElement() {
+//7 COMPLETE
+
+    var substituteElement = function (array, change, whatToChange) {
+        for(var i = 0; i < array.length; i++){
+             if (array[i] === change){
+                 array[i] = whatToChange;
+             }
+        }
+        return array;
     }
 
-//8
+    var result7 = substituteElement([123, 1, 153, 51, 1], 1, true )
+    console.log(result7); // undefined
 
-    function arraysHasElement() {
+//8 COMPLETE
+
+    var arraysHasElement = function (array, array, val) {
+        /*return array.some(function(arrVal) {
+            return val === arrVal;
+        });*/
+        for (var i = 0; i < array.length; i++){
+            if (array[i] === val){
+                return 'true';
+            }
+        }
+        return 'false';
     }
-
+    var result8 = arraysHasElement([1, 123, 513, 2312], [228, 1, 513, 512], 1);
+    console.log(result8);
 })()
